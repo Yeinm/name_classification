452ae2afb05cabc577a477aa4930d48371ce4836
必安git 谢 <zoomeeye@qq.com>
2024-04-22 12:05:10 +0800

    add

--- train.py
+++ train.py
@@ -149,24 +149,6 @@ plt.bar(range(len(x_data)),y_data,tick_label=x_data)
 '''
 
 #评估模型
-def evaluateRNN(line_tensor):
-    hidden = rnn.initHidden()
-    for i in range(line_tensor.size()[0]):
-        output, hidden = rnn(line_tensor[i], hidden)
-    return output.squeeze(0)
-
-def evaluateLSTM(line_tensor):
-    hidden,c= lstm.initHidden()
-    for i in range(line_tensor.size()[0]):
-        output, hidden, c = lstm(line_tensor[i],hidden,c)
-    return output.squeeze(0)
-
-def evaluateGRU(line_tensor):
-    hidden = gru.initHidden()
-    for i in range(line_tensor.size()[0]):
-        output, hidden = gru(line_tensor[i],hidden)
-    return output.squeeze(0)
-
 
 
 
